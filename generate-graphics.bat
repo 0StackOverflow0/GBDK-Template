da312045c345f822ff4f4085f18d50861d77beab
0StackOverflow0 <0johnconnor0@gmail.com>
2023-06-03 15:43:44 -0500

    Generate Graphics is now Dynamic
    Each png file will need a meta file matched with it; either a .sprite or .map extension

--- generate-graphics.bat
+++ generate-graphics.bat
@@ -1,16 +1,25 @@
-ECHO OFF
+@ECHO OFF
 
 SET GBDK=C:/gbdk
 SET PNG2ASSET=%GBDK%/bin/png2asset.exe
-:: following from ZGB but might be useful
-SET GBM2C=%GBDK%/bin/gbm2c.exe
-SET GBR2C=%GBDK%/bin/gbr2c.exe
 
 :: Sprites
-%PNG2ASSET% gfx\image.png -c gfx\image.c -b 2 -px 0 -py 0 -sx 0 -sy 0 -sw 32 -sh 32 -spr8x8 -noflip
+FOR /R "gfx" %%X IN (*.png) DO (
+    IF exist "%%X.sprite" (
+        FOR /F "tokens=*" %%i IN ('type "%%X.sprite"') DO (
+            %PNG2ASSET% %%X -c gfx/%%~nX.c %%i
+        )
+    )
+)
 
 :: Backgrounds & Maps
-%PNG2ASSET% gfx\map.png -b 2 -c gfx\map.c -map -use_map_attributes -noflip
+FOR /R "gfx" %%X IN (*.png) DO (
+    IF exist "%%X.map" (
+        FOR /F "tokens=*" %%i IN ('type "%%X.map"') DO (
+            %PNG2ASSET% %%X -c gfx\%%~nX.c %%i
+        )
+    )
+)
 
 :: move .h files to their proper location
 FOR /R "gfx" %%X IN (*.h) DO (
